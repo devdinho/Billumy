9181d8d259784226519bbe94051bf23ebd865a01
Anderson Freitas <freitas.dev@proton.me>
2025-04-23 12:06:17 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-export OLLAMA_HOST=0.0.0.0:11414
-
-ollama serve & sleep 5
 
 set -e
+
+export OLLAMA_HOST=0.0.0.0:11434
+
+ollama serve &
+until ollama list > /dev/null 2>&1; do
+    echo "Aguardando Ollama subir..."
+    sleep 1
+done
+
 if ! ollama list | grep -q "billumy"; then
     ollama create billumy -f /app/Modelfile
 fi
+
 wait
