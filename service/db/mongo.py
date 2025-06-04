5cba846a4b51e23b188dd4ed99f848cdb7c66b08
devdinho <freitas.dev@proton.me>
2025-06-04 02:33:49 -0300

    fix: Billumy

--- service/db/mongo.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from motor.motor_asyncio import AsyncIOMotorClient
-import os
-
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://billumy-mongo:27017")
-client = AsyncIOMotorClient(MONGO_URL)
-db = client["billumy"]
-chat_collection = db["chats"]
